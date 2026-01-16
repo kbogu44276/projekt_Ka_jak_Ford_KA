47b9af46257a3bcc98ef6b654785bd2bd54bb8eb
Anna Boguslawska <anna.boguslawska1991@gmail.com>
2026-01-16 15:23:33 +0100

    Add files via upload

--- app/controllers/MovieController.php
+++ app/controllers/MovieController.php
@@ -2,74 +2,51 @@
 
 class MovieController
 {
-    /**
-     * Strona główna (homepage.php)
-     */
+    private function getMoviesData()
+    {
+        return [
+            (object)['id' => 1, 'title' => 'Incepcja', 'rating' => 8.8, 'image' => 'https://bit.ly/3XkE7yA', 'desc' => 'Złodziej kradnie tajemnice korporacyjne poprzez technologię dzielenia się snami.'],
+            (object)['id' => 2, 'title' => 'Interstellar', 'rating' => 8.6, 'image' => 'https://bit.ly/4cmW3yB', 'desc' => 'Zespół badaczy podróżuje poza naszą galaktykę, by ratować ludzkość.'],
+            (object)['id' => 3, 'title' => 'Mroczny Rycerz', 'rating' => 9.0, 'image' => 'https://bit.ly/3VvUoN3', 'desc' => 'Batman walczy z Jokerem, który chce pogrążyć Gotham w chaosie.'],
+            (object)['id' => 4, 'title' => 'Pulp Fiction', 'rating' => 8.9, 'image' => 'https://bit.ly/3Xf9U7r', 'desc' => 'Przemoc i odkupienie w świecie przestępczym Los Angeles.'],
+            (object)['id' => 5, 'title' => 'Diuna: Część Druga', 'rating' => 8.1, 'image' => 'https://bit.ly/4aOshmE', 'desc' => 'Paul Atryda jednoczy się z Fremenami, by zemścić się na spiskowcach.'],
+            (object)['id' => 6, 'title' => 'Matrix', 'rating' => 8.7, 'image' => 'https://bit.ly/3x8mH8D', 'desc' => 'Haker komputerowy dowiaduje się o prawdziwej naturze swojej rzeczywistości.']
+        ];
+    }
+
     public function list(): void
     {
-        // 1. Statyczna lista filmów (nasza tymczasowa baza danych)
-        $allMovies = [
-            (object)['id' => 1, 'title' => 'Incepcja', 'rating' => 8.8, 'image' => 'https://bit.ly/3XkE7yA'],
-            (object)['id' => 2, 'title' => 'Interstellar', 'rating' => 8.6, 'image' => 'https://bit.ly/4cmW3yB'],
-            (object)['id' => 3, 'title' => 'Mroczny Rycerz', 'rating' => 9.0, 'image' => 'https://bit.ly/3VvUoN3'],
-            (object)['id' => 4, 'title' => 'Pulp Fiction', 'rating' => 8.9, 'image' => 'https://bit.ly/3Xf9U7r'],
-            (object)['id' => 5, 'title' => 'Diuna: Część Druga', 'rating' => 8.1, 'image' => 'https://bit.ly/4aOshmE'],
-            (object)['id' => 6, 'title' => 'Matrix', 'rating' => 8.7, 'image' => 'https://bit.ly/3x8mH8D']
-        ];
-
-        // 2. Obsługa wyszukiwarki
-        $query = $_GET['query'] ?? '';
-        if (!empty($query)) {
-            $allMovies = array_filter($allMovies, function($m) use ($query) {
-                return mb_stripos($m->title, $query) !== false;
-            });
-        }
-
-        // 3. Sekcja: Najwyżej oceniane (Sortowanie po ratingu)
-        $topRated = $allMovies;
-        usort($topRated, fn($a, $b) => $b->rating <=> $a->rating);
-        $topRated = array_slice($topRated, 0, 4);
-
-        // 4. Sekcja: Polecane (Losowa kolejność)
-        $recommended = $allMovies;
-        shuffle($recommended);
-        $recommended = array_slice($recommended, 0, 4);
-
-        // Renderowanie widoku z przekazaniem obu list
+        $allMovies = $this->getMoviesData();
         View::render('movie/homepage', [
-            'topRated' => $topRated,
-            'recommended' => $recommended
+            'topRated' => array_slice($allMovies, 0, 4),
+            'recommended' => $allMovies
         ]);
     }
 
-    /**
-     * Strona ulubionych filmów
-     */
-    public function favorites(): void
+    public function show(): void
     {
-        // Statyczna lista wybranych filmów dla widoku favorites.php
-        $favoriteMovies = [
-            (object)['id' => 1, 'title' => 'Incepcja', 'rating' => 8.8, 'image' => 'https://bit.ly/3XkE7yA'],
-            (object)['id' => 3, 'title' => 'Mroczny Rycerz', 'rating' => 9.0, 'image' => 'https://bit.ly/3VvUoN3']
-        ];
+        $id = $_GET['id'] ?? null;
+        $allMovies = $this->getMoviesData();
 
-        View::render('movie/favorites', ['movies' => $favoriteMovies]);
-    }
+        $selectedMovie = null;
+        foreach ($allMovies as $m) {
+            if ($m->id == $id) {
+                $selectedMovie = $m;
+                break;
+            }
+        }
 
-    /**
-     * Strona losowania filmu
-     */
-    public function random(): void
-    {
-        $allMovies = [
-            (object)['id' => 1, 'title' => 'Incepcja', 'rating' => 8.8, 'image' => 'https://bit.ly/3XkE7yA'],
-            (object)['id' => 2, 'title' => 'Interstellar', 'rating' => 8.6, 'image' => 'https://bit.ly/4cmW3yB'],
-            (object)['id' => 3, 'title' => 'Mroczny Rycerz', 'rating' => 9.0, 'image' => 'https://bit.ly/3VvUoN3']
-        ];
+        if (!$selectedMovie) {
+            header("Location: index.php");
+            exit;
+        }
 
-        // Wybór jednego losowego filmu
-        $randomMovie = $allMovies[array_rand($allMovies)];
+        // Przygotuj podobne filmy (wszystkie oprócz wybranego)
+        $similar = array_filter($allMovies, fn($m) => $m->id != $id);
 
-        View::render('movie/random', ['movie' => $randomMovie]);
+        View::render('movie/show', [
+            'movie' => $selectedMovie,
+            'similarMovies' => array_slice($similar, 0, 4)
+        ]);
     }
 }
