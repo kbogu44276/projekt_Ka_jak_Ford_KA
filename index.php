af5d3727b8d4060269de9acd1006dd68c02a4fae
Anna Boguslawska <anna.boguslawska1991@gmail.com>
2026-01-16 14:31:52 +0100

    Add files via upload
    widoki homepage

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php
+// Włącz wyświetlanie błędów, aby widzieć co się dzieje
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-require_once 'core/Database.php';
-require_once 'core/BaseModel.php';
-require_once 'core/View.php';
+define('BASE_DIR', __DIR__ . DIRECTORY_SEPARATOR);
 
-require_once 'app/models/Movie.php';
-require_once 'app/controllers/MovieController.php';
+// POPRAWIONE ŚCIEŻKI na podstawie Twojej struktury plików
+//require_once BASE_DIR . 'core' . DIRECTORY_SEPARATOR . 'Db' . DIRECTORY_SEPARATOR . 'Database.php';
+require_once BASE_DIR . 'core' . DIRECTORY_SEPARATOR . 'BaseModel.php';
+require_once BASE_DIR . 'core' . DIRECTORY_SEPARATOR . 'View.php';
+
+require_once BASE_DIR . 'app' . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'Movie.php';
+require_once BASE_DIR . 'app' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'MovieController.php';
+require_once BASE_DIR . 'app' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'adminController.php';
 
 $controller = $_GET['controller'] ?? 'movie';
 $action = $_GET['action'] ?? 'list';
 
 $controllerClass = ucfirst($controller) . 'Controller';
 
-$instance = new $controllerClass();
-$instance->$action();
+if (class_exists($controllerClass)) {
+    $instance = new $controllerClass();
+    if (method_exists($instance, $action)) {
+        $instance->$action();
+    } else {
+        die("Akcja $action nie istnieje.");
+    }
+} else {
+    die("Kontroler $controllerClass nie istnieje.");
+}
