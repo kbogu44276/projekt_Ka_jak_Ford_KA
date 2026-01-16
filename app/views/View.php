4b6620325fe9d4b41ada5d42f32fc10ad5f02df4
Anna Boguslawska <anna.boguslawska1991@gmail.com>
2026-01-16 14:51:09 +0100

    Add files via upload

---
+++ app/views/View.php
@@ -0,0 +1,11 @@
+<?php
+
+class View
+{
+    public static function render(string $view, array $data = []): void
+    {
+        extract($data);
+        // Pliki będą w app/views/ (np. app/views/movie/homepage.php)
+        require "app/views/$view.php";
+    }
+}
